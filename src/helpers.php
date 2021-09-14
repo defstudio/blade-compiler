4ea7c45a63741662d3d5408bf8c2691832711233
Fabio Ivona <fabio.ivona@defstudio.it>
2021-09-14 15:57:51 +0200

    fix

--- src/helpers.php
+++ src/helpers.php
@@ -1,35 +1,13 @@
 <?php
 
 use App\Models\User;
+use DefStudio\BladeCompiler\Helpers\BladeCompiler;
 use Illuminate\Http\RedirectResponse;
 
-if (!function_exists('user')) {
-    function user(): User|null
-    {
-        return Auth::user();
-    }
-}
 
-if (!function_exists('str')) {
-    /**
-     * Get a new stringable object from the given string.
-     *
-     * @param  string|\Illuminate\Support\Stringable  $string  $string
-     *
-     * @return \Illuminate\Support\Stringable
-     */
-    function str(null|string|\Illuminate\Support\Stringable $string): \Illuminate\Support\Stringable
+if (!function_exists('blade')) {
+    function blade(): BladeCompiler
     {
-        return \Illuminate\Support\Str::of($string ?? '');
-    }
-}
-
-if (!function_exists('back')) {
-    function back($status = 302, $headers = [], $fallback = false): RedirectResponse
-    {
-        if (request()->has('referer')) {
-            return app('redirect')->to(request()->referer, $status, $headers);
-        }
-        return app('redirect')->back($status, $headers, $fallback);
+        return app(BladeCompiler::class);
     }
 }
