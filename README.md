1d694cf8f557158ed9fe4e595ed7326a85c1d9fb
Fabio Ivona <fabio.ivona@defstudio.it>
2021-09-14 15:53:42 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Blade Compiler
+
+## Installation
+You can install this package via Composer:
+
+`composer require defstudio/blade-compiler`
+
+## Usage
+
+```blade
+$compiled_html = blade()->render('{html}/{view name}', [...data])
+```
---
+++ src/Helpers/BladeCompiler.php
@@ -0,0 +1,46 @@
+<?php /** @noinspection PhpUnhandledExceptionInspection */
+
+namespace DefStudio\BladeCompiler\Helpers;
+
+use Exception;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\View;
+use Throwable;
+
+class BladeCompiler
+{
+    public function render($content, $data): string
+    {
+        if (View::exists($content)) {
+            return View::make($content, $data)->render();
+        }
+
+        $php = Blade::compileString($content);
+
+        $obLevel = ob_get_level();
+        ob_start();
+        extract($data, EXTR_SKIP);
+
+        try {
+            eval('?'.'>'.$php);
+        } catch (Exception $e) {
+            while (ob_get_level() > $obLevel) {
+                ob_end_clean();
+            }
+            throw $e;
+        } catch (Throwable $e) {
+            while (ob_get_level() > $obLevel) {
+                ob_end_clean();
+            }
+            throw new Exception($e);
+        }
+
+        $html = ob_get_clean();
+
+        if (!$html) {
+            $html = '';
+        }
+
+        return $html;
+    }
+}
